f58425247ff9c54b7431ab747ef34337a484fa52
gt <jack139@gmail.com>
2023-04-09 15:34:05 +0800

    update

--- sample.py
+++ sample.py
@@ -23,7 +23,7 @@ compile = False # use PyTorch 2.0 to compile the model to be faster
 exec(open('configurator.py').read()) # overrides from command line or config file
 # -----------------------------------------------------------------------------
 
-ckpt_file = 'ckpt-val-loss_4.31853.pt'
+ckpt_file = 'ckpt-val-loss_2.98983.pt'
 
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
